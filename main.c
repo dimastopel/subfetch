f2ff42fc6d94513230481517dab19c749a9bc701
Dima Stopel <dima@stopel.org>
2011-05-25 22:57:12 +0300

    still prototyping...

--- main.c
+++ main.c
@@ -1,44 +1,4 @@
-/* An XML-RPC client program written in C, as an example of using
-   compound XML-RPC values.
-
-   For a simple client program that just deals with integer values,
-   see xmlrpc_sample_add_client.c.  This example focuses just on the
-   compound XML-RPC values and not the client functions.
-
-   This client invokes the example.divide XML-RPC method that the example
-   server program compound_value_server.c provides.  That method takes a
-   list of pairs of numbers and returns the list of their quotients.
-
-   Compound XML-RPC values are arrays and structures.  We call them compound
-   because they are made up of other XML-RPC values (e.g. an array of XML-RPC
-   integers).
-
-   The arguments to the example.divide method are specified as follows:
-
-   There are two arguments:
-
-     Argument 0: Integer.  Version number of this argument protocol.  Must
-                 be 1.
-
-
-     Argument 1: Array.  One element for each pair of numbers you want the
-                 server to divide.  Each element is structure, with these
-                 members:
-
-                 KEY: "dividend"
-                 VALUE: floating point number.  The dividend.
-
-                 KEY: "divisor"
-                 VALUE: floating point number.  The divisor.
-
-   The result of the method is an array.  It has one member for each pair of
-   numbers in the arguments (So it is the same size as Argument 1).  That
-   member is a floating point number.  It is the quotient of the numbers
-   in the corresponding element of Argument 1.
-
-   The client sends the RPC to the server running on the local system
-   ("localhost"), HTTP Port 8080.
-*/
+#include "common.h"
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -46,10 +6,12 @@
 #include <xmlrpc-c/base.h>
 #include <xmlrpc-c/client.h>
 
+#include "utils.h"
+
 //#include "config.h"  /* information about this build environment */
 
-#define NAME "subfetch"
-#define VERSION "0.1"
+#define PROG_NAME "subfetch"
+#define PROG_VERSION "0.1"
 
 static void 
 dieIfFaultOccurred (xmlrpc_env * const envP) {
@@ -60,28 +22,21 @@ dieIfFaultOccurred (xmlrpc_env * const envP) {
     }
 }
 
-
-
-struct ratio {
-    double dividend;
-    double divisor;
-};
-
-
-
 int 
 main(int           const argc, 
      const char ** const argv) {
 
     const char * const serverUrl = "http://api.opensubtitles.org/xml-rpc";
     const char * const methodName = "LogIn";
+    const char * const defLang = "eng";
     unsigned int const argVersion = 1;
-    struct ratio const data[] = {{1,2},{12,3},{10,3},{89,3000}};
     xmlrpc_env env;
-    xmlrpc_value * resultP;
+    xmlrpc_value * resultP, * searchSubsReq, *resultP2;
     unsigned int i;
     xmlrpc_value * ratioArrayP;
     unsigned int quotientCt;
+    char * token = NULL;
+
 
     if (argc-1 > 0) {
         fprintf(stderr, "This program has no arguments\n");
@@ -92,42 +47,56 @@ main(int           const argc,
     xmlrpc_env_init(&env);
     printf("Init successful\n");
 
-    xmlrpc_client_init2(&env, XMLRPC_CLIENT_NO_FLAGS, NAME, VERSION, NULL, 0);
+    xmlrpc_client_init2(&env, XMLRPC_CLIENT_NO_FLAGS, PROG_NAME, PROG_VERSION, NULL, 0);
     dieIfFaultOccurred(&env);
     printf("Init2 successful\n");
 
+    /*
     xmlrpc_value* s1 = xmlrpc_string_new(&env, "");
     xmlrpc_value* s2 = xmlrpc_string_new(&env, "");
     xmlrpc_value* s3 = xmlrpc_string_new(&env, "eng");
     xmlrpc_value* s4 = xmlrpc_string_new(&env, "oscar v2.13");
-    printf("Initialized variables\n");
+    */
     
-    /* Make the call */
+    //
+    // Get the token - LogIn
+    //
 
     printf("Making the call\n");
-    resultP = xmlrpc_client_call(&env, serverUrl, methodName, "(ssss)",
-                                 "","","eng","oscar v2.13");
+    //resultP = xmlrpc_client_call(&env, serverUrl, methodName, "(ssss)",
+    //                             "","","eng","oscar v2.13");
+    resultP = xmlrpc_client_call(&env, 
+				 serverUrl, 
+				 methodName, 
+				 "(ssss)",
+                                 "",
+				 "",
+				 defLang,
+				 PROG_NAME " " PROG_VERSION);
     dieIfFaultOccurred(&env);
     printf("after call\n");
     
        
     /* Print out the quotients returned */
 
-    printf("the type is: %d", xmlrpc_value_type(resultP));
+    printf("the type is: %d\n", xmlrpc_value_type(resultP));
     
     
-    xmlrpc_value * token;
+    xmlrpc_value * token1;
     xmlrpc_value * status;
     xmlrpc_value * seconds;
 
-    xmlrpc_struct_find_value(&env, resultP, "token", &token);
+    xmlrpc_struct_find_value(&env, resultP, "token", &token1);
     xmlrpc_struct_find_value(&env, resultP, "status", &status);
     xmlrpc_struct_find_value(&env, resultP, "seconds", &seconds);
 
-    if (token) {
+
+
+    if (token1) {
         const char* token_val;
-        xmlrpc_read_string(&env, token, &token_val);
+        xmlrpc_read_string(&env, token1, &token_val);
         printf("token is %s\n", token_val);
+	token = token_val;
     } 
     else
     {
@@ -155,53 +124,60 @@ main(int           const argc,
         printf("There is no member named seconds\n");
     }
     
-    int size = xmlrpc_struct_size(&env, resultP);
-    printf("struct size is: [%d]\n",size);
-    int counter;
-    for (counter = 0; counter < size; counter++)
-    {
-	
-        xmlrpc_value * keyval;
-        xmlrpc_value * valval;
-	xmlrpc_struct_read_member(&env,
-                          resultP,
-                          counter,
-                          &keyval,
-                          &valval);
-
-        const char* string_val;
-        xmlrpc_read_string(&env, keyval, &string_val);
-        printf("key [%d] is %s\n", counter,string_val);
-	
-
-    }
     
-	xmlrpc_DECREF(resultP);
-
-    
-    
-    /*
-    quotientCt = xmlrpc_array_size(&env, resultP);
-    dieIfFaultOccurred(&env);
-    printf("array size: %d", quotientCt);
-
-    for (i = 0; i < quotientCt; ++i) {
-        xmlrpc_value * quotientP;
-        xmlrpc_double quotient;
-
-        xmlrpc_array_read_item(&env, resultP, i, &quotientP);
-        dieIfFaultOccurred(&env);
-
-        xmlrpc_read_double(&env, quotientP, &quotient);
-        dieIfFaultOccurred(&env);
-
-        printf("Server says quotient %u is %f\n", i, quotient);
-
-        xmlrpc_DECREF(quotientP);
-    }
-
     xmlrpc_DECREF(resultP);
-	*/
+
+    //
+    // SErach for subtitles
+    //
+
+    //Create struct
+    
+
+    //Create request
+    searchSubsReq = xmlrpc_build_value(&env, 
+                                   "({s:s,s:s,s:i})", 
+				   "sublanguageid", "eng",
+                                   "moviehash", "68a9ccba52f5b1bf",
+                                   "moviebytesize", 585347231);
+
+
+
+
+
+    resultP2 = xmlrpc_client_call(&env, 
+				 serverUrl, 
+				 "SearchSubtitles", 
+				 "(sA)",
+				  token,
+				 searchSubsReq);
+
+    dieIfFaultOccurred(&env);
+    printf("after call\n");
+    
+    printf("the type is: %d\n", xmlrpc_value_type(resultP2));
+    
+    
+    const char * status_key, * status_val, * data_key;
+    xmlrpc_value * dataArray;
+    
+    xmlrpc_decompose_value(&env, resultP2, "{s:s,s:A}",
+                           &status_key,
+                           &status_val,
+                           &data_key,
+			   &dataArray);
+
+    printf("status: key[%s] val[%s]", status_key, status_val);
+    printf("data: key[%s]", data_key);
+    
+    unsigned int dataSize = xmlrpc_array_size(&env, dataArray);
+    for (i = 0; i < dataSize; i++)
+      {
+	xmlrpc_value * val = NULL;
+	xmlrpc_array_read_item(&env, dataArray, i, &val);
+	PrintXmlRpcStruct(&env, val);
+      }
+
     xmlrpc_env_clean(&env);
     
     xmlrpc_client_cleanup();
--- makefile
+++ makefile
@@ -1,28 +1,21 @@
+
+CC=gcc
+CFLAGS=-c `xmlrpc-c-config --cflags`
+LIBS=`xmlrpc-c-config client --libs`
+
 all: subfetch
 
-subfetch: main.o
-	gcc `xmlrpc-c-config client --libs` main.o -o subfetch
+subfetch: main.o utils.o 
+	$(CC) $(LIBS) main.o utils.o -o subfetch
 
 main.o: main.c
-	gcc -c `xmlrpc-c-config --cflags` main.c
+	$(CC) $(CFLAGS) main.c
+
+utils.o: utils.c
+	$(CC) $(CFLAGS) utils.c
 
 clean:
-	rm -rf *o subfetch
-
-	
-	
-	
-	
-#client	: xmlrpc_sample_client.cpp
-#	g++ `xmlrpc-c-config --cflags` -g -O2 -W -Wall -Wmissing-prototypes -Wstrict-prototypes -DPROTOTYPES -o client xmlrpc_sample_client.cpp `xmlrpc-c-config --libs` -lxmlrpc_client -pthread
-
-#covit : send_rtp.cpp recv_rtp.cpp unicapVidCap.cpp videoenc.cpp capture.cpp xmlrpc_server.cpp
-#	g++ `xmlrpc-c-config --cflags` -g -O2 -W -Wall -Wmissing-prototypes -Wstrict-prototypes -DPROTOTYPES -o covit.o send_rtp.cpp recv_rtp.cpp unicapVidCap.cpp videoenc.cpp capture.cpp xmlrpc_server.cpp librtpunix.a librtp.a -lm -lnsl -lSDL -lavformat -lavcodec -lz -lavutil `xmlrpc-c-config --libs` -lxmlrpc_server_abyss -pthread `pkg-config --cflags --libs libunicap`
+	rm -rf *.o subfetch
 
 
 
-#all: subfetch
-#subfetch : main.cpp
-#	gcc `xmlrpc-c-config --cflags` -o subfetch `xmlrpc-c-config client c++2 --libs` main.cpp
-#clean:	
-#	rm -f subfetch *.o
